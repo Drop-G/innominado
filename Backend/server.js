bafb14cc80bb98d6b481972601a22674860ac814
Drop-G <tedpetersiv@gmail.com>
2021-02-08 14:19:08 -0600

    connected to atlas

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,12 @@
 const express = require ('express');
 const mongoose = require ('mongoose');
 const app = express()
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Innominado", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connection.on('connected', function(){ console.log("Connected to MongoDB") });
+mongoose.connection.on('error', function (err) { console.log(err) });
+mongoose.connection.on('disconnected', function(){console.log("Disconnected from MongoDB")})
 
 app.get('/', function(req,res){
     res.send("Hello Space")
