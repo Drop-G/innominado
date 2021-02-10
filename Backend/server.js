d9e6542066ff5c1d108f7cd66602efe85672004c
Drop-G <tedpetersiv@gmail.com>
2021-02-09 19:10:11 -0600

    backend setup

--- Backend/server.js
+++ Backend/server.js
@@ -2,16 +2,37 @@ const express = require ('express');
 const mongoose = require ('mongoose');
 const app = express()
 require('dotenv').config();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const errorHandler = require('../_middleware/error-handler');
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Innominado", {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.connection.on('connected', function(){ console.log("Connected to MongoDB") });
 mongoose.connection.on('error', function (err) { console.log(err) });
 mongoose.connection.on('disconnected', function(){console.log("Disconnected from MongoDB")})
 
-app.get('/', function(req,res){
-    res.send("Hello Space")
-})
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log('Server Listening on PORT 3000')
-})
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// allow cors requests from any origin and with credentials
+app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+
+// api routes
+app.use('/accounts', require('../accounts/accounts.controller'));
+
+// swagger docs route
+app.use('/api-docs', require('../_helpers/swagger'));
+
+// global error handler
+app.use(errorHandler);
+
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3001;
+app.listen(port, () => {
+    console.log('Server listening on port ' + port);
+});
---
+++ _helpers/db.js
@@ -0,0 +1,19 @@
+const config = require('../config.json');
+const mongoose = require('mongoose');
+require ('dotenv').config();
+
+const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
+mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions);
+mongoose.connection.on('connected', function(){console.log('Connected to MongoDB Admin')})
+
+mongoose.Promise = global.Promise;
+
+module.exports = {
+    Account: require('../accounts/account.model'),
+    RefreshToken: require('../accounts/refresh-token.model'),
+    isValidId
+};
+
+function isValidId(id) {
+    return mongoose.Types.ObjectId.isValid(id);
+}
