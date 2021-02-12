e21ad41a16f6475b69f4036d832b4c361fba037a
Drop-G <tedpetersiv@gmail.com>
2021-02-12 11:32:56 -0600

    running

--- Backend/server.js
+++ Backend/server.js
@@ -7,10 +7,6 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const errorHandler = require('../_middleware/error-handler');
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Innominado", {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connection.on('connected', function(){ console.log("Connected to MongoDB") });
-mongoose.connection.on('error', function (err) { console.log(err) });
-mongoose.connection.on('disconnected', function(){console.log("Disconnected from MongoDB")})
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
@@ -32,7 +28,7 @@ app.use('/api-docs', require('../_helpers/swagger'));
 // global error handler
 app.use(errorHandler);
 
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3001;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 app.listen(port, () => {
     console.log('Server listening on port ' + port);
 });
